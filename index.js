d1dcdda426b48c5b55701605b6e41f686c2ab1c8
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-09-05 23:26:38 +0300

    add PUG and create Views

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('views', './views') //Tìm đến thư mục views
+app.set('view engine', 'pug') // đọc template engine
+
 app.get('/', (req, res) => {
-  res.send("Trang chủ")
+  res.render("client/pages/home/index.pug")
 })
 
 app.get('/products', (req, res) => {
-  res.send("Trang danh sách sản phẩm")
+  res.render("client/pages/products/index.pug")
 })
 
 app.listen(port, () => {
