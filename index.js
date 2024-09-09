f69d47b38d77e342df5d3eb3617e3d26c75dc4f1
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-09-09 13:02:37 +0300

    config static file 'public'

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const routeClient = require("./routes/client/index.route")
 app.set('views', './views') //Tìm đến thư mục views
 app.set('view engine', 'pug') // đọc template engine
 
+app.use(express.static('public')) // Thiết lập thư mục chứa file tĩnh
+
 routeClient.index(app)
 
 app.listen(port, () => {
