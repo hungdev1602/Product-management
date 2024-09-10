8349824663ad679786a7c897573b0872224e00e6
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-09-10 23:00:54 +0300

    thêm biến toàn cục prefixAdmin

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT;
 
+const systemConfig = require('./config/system')
+
 // Connect database
 const database = require("./config/database")
 database.connect()
@@ -16,6 +18,10 @@ app.set('view engine', 'pug') // đọc template engine
 
 app.use(express.static('public')) // Thiết lập thư mục chứa file tĩnh
 
+// Khai báo biến toàn cục cho file PUG
+app.locals.prefixAdmin = systemConfig.prefixAdmin
+
+// Khai báo các route (đường dẫn)
 routeAdmin.index(app)
 routeClient.index(app)
 
