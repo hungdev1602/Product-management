3a1fcef13b97ac6129a180338c444061dfdd0a77
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-09-10 23:09:22 +0300

    Lấy data sản phẩm in ra giao diện Admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,11 @@
-module.exports.index = (req, res) => {
+const Product = require('../../models/product.model')
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    deleted: false
+  })
+
   res.render("admin/pages/product/index.pug", {
-    pageTitle: "Danh sach san pham"
+    pageTitle: "Danh sach san pham",
+    products: products
   })
 }
