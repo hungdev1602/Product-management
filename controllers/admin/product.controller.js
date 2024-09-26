c39d435f00ec34f7ad113bdc47e33f7edbbf6077
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-09-26 19:43:35 +0300

    Connect MongoDB Cloud Online

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -185,8 +185,6 @@ module.exports.createPost = async (req, res) => {
     req.body.thumbnail = `/uploads/${req.file.filename}`
   }
 
-  res.send("OK")
-
   // lưu vào DB
   const record = new Product(req.body);
   await record.save();
