8bf1d65365c16d94ecb69617331d7608216561cd
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-09-28 13:19:35 +0300

    Tạo danh mục sản phẩm

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,5 +1,7 @@
 const dashboardRoute = require('./dashboard.route')
 const productRoute = require('./product.route')
+const productCategoryRoute = require('./products-category.route')
+
 const systemConfig = require('../../config/system')
 module.exports.index = (app) => {
   const PATH_ADMIN = `/${systemConfig.prefixAdmin}`
@@ -7,4 +9,6 @@ module.exports.index = (app) => {
   app.use(`${PATH_ADMIN}/dashboard`, dashboardRoute)
 
   app.use(`${PATH_ADMIN}/products`, productRoute)
+
+  app.use(`${PATH_ADMIN}/products-category`, productCategoryRoute)
 }
