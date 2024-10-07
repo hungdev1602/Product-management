8631aed3e3086428b071194e17413d14b92c9646
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-10-07 11:06:32 +0300

    Xây dựng phần Thêm mới nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,5 +1,5 @@
 const Role = require("../../models/role.model")
-
+const systemConfig = require('../../config/system')
 module.exports.index = async (req, res) => {
   const records = await Role.find({
     deleted: false
@@ -9,4 +9,18 @@ module.exports.index = async (req, res) => {
     pageTitle: "Nhóm quyền",
     records: records
   })
+}
+
+module.exports.create = async (req, res) => {
+
+  res.render("admin/pages/roles/create.pug", {
+    pageTitle: "Tạo nhóm quyền"
+  })
+}
+
+module.exports.createPost = async (req, res) => {
+  const role = new Role(req.body);
+  await role.save();
+  
+  res.redirect(`/${systemConfig.prefixAdmin}/roles`)
 }
