af24d5321fda0c6d57ebc2e058eae66c5a15a15d
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-10-26 20:33:00 +0300

    Trang danh sách sản phẩm bên client - Lấy sản phẩm đúng danh mục

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,3 +1,4 @@
+const ProductCategory = require("../../models/product-category.model");
 const Product = require("../../models/product.model")
 
 module.exports.index = async (req, res) => {
@@ -38,5 +39,35 @@ module.exports.detail = async (req, res) => {
   res.render("client/pages/products/detail.pug", {
     pageTitle: product.title,
     product: product
+  })
+}
+
+module.exports.category = async (req, res) => {
+  const slugCategory = req.params.slugCategory;
+
+  const category = await ProductCategory.findOne({
+    slug: slugCategory,
+    deleted: false,
+    status: "active"
+  })
+
+  const products = await Product.find({
+    category_id: category._id,
+    status: "active",
+    deleted: false
+  })
+    .sort({
+      position: "desc"
+    })
+
+  for (const item of products) {
+    const discountPrice = item.price * item.discountPercentage / 100;
+    item.priceNew = (item.price - discountPrice).toFixed(0)
+  }
+  
+  
+  res.render("client/pages/products/index.pug", {
+    pageTitle: `Sản phẩm ${category.title}`,
+    products: products
   })
 }
