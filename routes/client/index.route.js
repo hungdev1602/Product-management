27b5f10c2d7c6426b23ade792b0d6d7dcf2aa52d
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-10-26 19:18:50 +0300

    Hiển thị menu chung bên client (làm các danh mục phân cấp ở menu)

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,6 +1,10 @@
 const productRoute = require("./product.route")
 const homeRoute = require("./home.route")
+
+const categoryMiddleware = require("../../middlewares/client/category.middleware")
 module.exports.index = (app) => {
+  app.use(categoryMiddleware.category); //Tất cả các router đều chạy qua middleware này (cách viết tắt)
+
   app.use('/', homeRoute)
   
   app.use('/products', productRoute)
